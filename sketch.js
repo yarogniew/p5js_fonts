ec346f784b48940dc851b66bce2b86dda48707ea
yarogniew <1@yarogniew.net>
2018-03-09 23:40:38 +0100

    po czyszczeniu dz.

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 var myFont;
 var x=360;
+var xoff = 0.05;
+var xincrement = 0.01;
+
 function preload() {
   myFont = loadFont('Sansation_Regular.ttf');
 }
 
 function setup() {
   createCanvas(480, 320);
-  background('white');
-  fill('grey');
+  //background('black');
+  fill('white');
   textFont(myFont);
   textSize(16);
   //text('gąwno!', 10, 50);
@@ -15,10 +18,14 @@ function setup() {
 
 function draw() {
   if (x>10)
-  {  background('white');
+  {  background('grey');
+  var n = noise(xoff)*10;
 
-    text('dłuższy tekst, który ma się pojawić!', x, 50);
-    x-=5;
+  // With each cycle, increment xoff
+  xoff = random(0,0.1);
+    //text('dłuższy tekst, który ma się pojawić!', x, 50+n*300);
+        text(n, x, height/2);
+    x-=n;
 }
 
 
