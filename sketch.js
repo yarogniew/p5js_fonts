9dc1efa876eea507be45c5b6157affb335919c28
yarogniew <1@yarogniew.net>
2018-03-09 13:45:33 +0100

    dzialajaca wersja wjazd tekstu

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
 var myFont;
+var x=360;
 function preload() {
-  myFont = loadFont('assets/AvenirNextLTPro-Demi.otf');
+  myFont = loadFont('Sansation_Regular.ttf');
 }
 
 function setup() {
+  createCanvas(360, 480);
+  background('white');
   fill('#ED225D');
   textFont(myFont);
   textSize(36);
-  text('p5*js', 10, 50);
+  //text('gąwno!', 10, 50);
+}
+
+function draw() {
+  if (x>100)
+  {  background('white');
+
+    text('gąwno!', x, 50);
+    x-=5;
+}
+
+
 }
